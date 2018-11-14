0de040f0d50c25362ddf9ba80254ccbc5d185b2a
Wilson Nie <wilson.nie13@gmail.com>
2018-11-14 14:27:20 -0800

    circular progress bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,125 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class CircularProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  render() {
+    // Size of the enclosing square
+    const sqSize = this.props.sqSize;
+    // SVG centers the stroke width on the radius, subtract out so circle fits in square
+    const radius = (this.props.sqSize - this.props.strokeWidth) / 2;
+    // Enclose cicle in a circumscribing square
+    const viewBox = `0 0 ${sqSize} ${sqSize}`;
+    // Arc length at 100% coverage is the circle circumference
+    const dashArray = radius * Math.PI * 2;
+    // Scale 100% coverage overlay with the actual percent
+    const dashOffset = dashArray - (dashArray * this.props.percentage) / 100;
+
+    return (
+      <svg
+        width={this.props.sqSize}
+        height={this.props.sqSize}
+        viewBox={viewBox}
+      >
+        <circle
+          className="circle-background"
+          cx={this.props.sqSize / 2}
+          cy={this.props.sqSize / 2}
+          r={radius}
+          strokeWidth={`${this.props.strokeWidth}px`}
+        />
+        <circle
+          className="circle-progress"
+          cx={this.props.sqSize / 2}
+          cy={this.props.sqSize / 2}
+          r={radius}
+          strokeWidth={`${this.props.strokeWidth}px`}
+          // Start progress marker at 12 O'Clock
+          transform={`rotate(-90 ${this.props.sqSize / 2} ${this.props.sqSize /
+            2})`}
+          style={{
+            strokeDasharray: dashArray,
+            strokeDashoffset: dashOffset
+          }}
+        />
+        <text
+          className="circle-text"
+          x="50%"
+          y="50%"
+          dy=".3em"
+          textAnchor="middle"
+          id="mytext"
+        >
+          {`word`}
+        </text>
+      </svg>
+    );
+  }
+}
+
+CircularProgressBar.defaultProps = {
+  sqSize: 200,
+  percentage: 25,
+  strokeWidth: 10
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      percentage: 25
+    };
+
+    this.handleChangeEvent = this.handleChangeEvent.bind(this);
+  }
+
+  handleChangeEvent(event) {
+    let x = event.target.value;
+    var word;
+    if (x > 75 && x < 80) {
+      this.word = "Capable";
+    } else if (x > 80 && x < 85) {
+      this.word = "Excllent";
+    } else if (x > 85) {
+      this.word = "Experience";
+    } else {
+      this.word = "meh";
+    }
+
+    this.setState({ percentage: event.target.value });
+    document.getElementById("mytext").innerHTML = this.word;
+    console.log(this.word);
+  }
+
+  render() {
+    return (
+      <div>
+        <CircularProgressBar
+          strokeWidth="10"
+          sqSize="200"
+          percentage={this.state.percentage}
+        />
+        <div>
+          <input
+            id="progressInput"
+            type="range"
+            min="0"
+            max="100"
+            step="1"
+            value={this.state.percentage}
+            onChange={this.handleChangeEvent}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
